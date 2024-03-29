159c48f01681ffd32dd51ba3489e0caa70a2b170
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-03-29 11:28:23 +0530

    Add CLI input

--- producer.js
+++ producer.js
@@ -1,4 +1,10 @@
 const {kafka} = require('./client')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 async function init() {
     const producer = kafka.producer()
@@ -6,18 +12,24 @@ async function init() {
     await producer.connect()
     console.log('producer connected')
 
-    await producer.send({
-        topic: 'messages',
-        messages: [
-            { 
-                partition: 0,
-                key:'message-send', 
-                value: JSON.stringify({ user: 'Nishanth', message: 'Hi, How are you?'})}
-        ]
-    })
+    rl.setPrompt('> ')
+    rl.prompt()
 
-    producer.disconnect()
-    console.log('producer disconnected')
+    rl.on('line', async function(line) {
+        const [userName, userMessage, partition] = line.split(' ')
+        await producer.send({
+            topic: 'messages',
+            messages: [
+                { 
+                    partition: partition,
+                    key:'message-send', 
+                    value: JSON.stringify({ user: userName, message: userMessage})}
+            ]
+        })
+    }).on('close', async function() {
+        await producer.disconnect()
+        console.log('producer disconnected')
+    })
 }
 
 init()
